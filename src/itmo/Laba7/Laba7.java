f814bd56deeb822dc917f8f51bd4857d097e25f9
DLapinM <autocad-profi@yandex.ru>
2024-04-25 21:50:08 +0300

    Laba7 finish

--- src/itmo/Laba7/Laba7.java
+++ src/itmo/Laba7/Laba7.java
@@ -1,4 +1,13 @@
 package itmo.Laba7;
 
-public class Laba7 {
+import java.util.Calendar;
+
+public class Laba7
+{
+    public static void main(String[] args)
+    {
+        Plane plane1 = new Plane(5, 300, 30000, 2, 0.5, 20, 2, 0.5, 25);
+
+        plane1.ShowMassOfWings();
+    }
 }
